cdaecb63e185124d749ea19500f642ded901f5b9
RaidAndFade <business@gocode.it>
2018-06-27 02:14:44 -0400

    Change map namespace to banshee, Finalize map init
    Map initializer correctly finds and calculates CRC, INFO, and SHA1 for all tested maps.

--- src/Map.cs
+++ src/Map.cs
@@ -1,20 +1,26 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 
 using Nito.KitchenSink.CRC;
 
 using Foole.Mpq;
 
-namespace WC3_PROTOCOL
+namespace Banshee
 {
     public class Map
     {
         byte[] mapData;
-        int mapSize;
-        byte[] crc;
+
+        //shit figured out from mpq
+        byte[] MapSize;
+        byte[] MapInfo;
+        byte[] MapCRC;
+        byte[] MapSha;
 
         public Map(string filename)
         {
@@ -23,29 +29,106 @@ namespace WC3_PROTOCOL
                 fs.Read(d, 0, d.Length);
                 fs.Seek(0,SeekOrigin.Begin);
 
-                crc = new CRC32().ComputeHash(d, 0, d.Length);
-                Console.WriteLine(string.Join(',',crc));
+                MapInfo = new CRC32().ComputeHash(d, 0, d.Length);
 
                 using (MpqArchive a = new MpqArchive(fs,true))
                 {
-                    string[] filenames = { "(listfile)", "war3map.j", "scripts\\war3map.j", "war3map.w3e", "war3map.wpm", "war3map.doo", "war3map.w3u", "war3map.w3b", "war3map.w3d", "war3map.w3a", "war3map.w3q" };
+                    uint CRCVal = 0;
+                    
+                    SHA1Managed sha = new SHA1Managed();
+
+                    byte[] commonJData, blizzardJData;
+                    if(a.FileExists("Scripts\\common.j")){
+                        using(var commonOverload = a.OpenFile("Scripts\\common.j")){
+                            var ms = new MemoryStream();
+                            commonOverload.CopyTo(ms);
+                            commonJData = ms.ToArray();
+                            ms.Dispose();
+                        }
+                    }else{
+                        commonJData = File.ReadAllBytes("dep/common.j");
+                    }
+                    if(a.FileExists("Scripts\\blizzard.j")){
+                        using(var blizzardOverload = a.OpenFile("Scripts\\blizzard.j")){
+                            var ms = new MemoryStream();
+                            blizzardOverload.CopyTo(ms);
+                            blizzardJData = ms.ToArray();
+                            ms.Dispose();
+                        }
+                    }else{
+                        blizzardJData = File.ReadAllBytes("dep/blizzard.j");
+                    }
+
+                    CRCVal = CRCVal ^ XORRotateLeft(commonJData);
+                    CRCVal = CRCVal ^ XORRotateLeft(blizzardJData);
+
+                    sha.TransformBlock(commonJData,0,commonJData.Length,commonJData,0);
+                    sha.TransformBlock(blizzardJData,0,blizzardJData.Length,blizzardJData,0);
+
+                    var magicBytes = new byte[]{0x9e,0x37,0xf1,0x03};
+                    uint magicInt = 0x03F1379E;
+
+                    CRCVal = ROTL(CRCVal, 3);
+                    CRCVal = ROTL(CRCVal ^ magicInt, 3);
+
+                    sha.TransformBlock(magicBytes,0,magicBytes.Length,magicBytes,0);
+                    
+                    string[] filenames = { "war3map.j", "scripts\\war3map.j", "war3map.w3e", "war3map.wpm", "war3map.doo", "war3map.w3u", "war3map.w3b", "war3map.w3d", "war3map.w3a", "war3map.w3q" };
+                    var foundScript = false;
+                    
                     foreach (string fn in filenames)
                     {
-                        try
-                        {
-                            a.AddFilename(fn);
+                        if(foundScript && fn == filenames[2]) continue;
+
+                        if(!a.FileExists(fn)) continue;
+
+                        using(MpqStream s = a.OpenFile(fn)){
+                            var ms = new MemoryStream();
+                            s.CopyTo(ms);
+                            var fdata = ms.ToArray();
+                            ms.Dispose();
+
+                            CRCVal = ROTL(CRCVal ^ XORRotateLeft(fdata),3);
+                            sha.TransformBlock(fdata,0,fdata.Length,fdata,0);
                         }
-                        catch (Exception e) { }
                     }
 
+                    MapCRC = BitConverter.GetBytes(CRCVal);
 
-                    for (int i = 0; i < a.Count; i++)
-                    {
-                        Console.WriteLine(a[i]);
-                    }
+                    sha.TransformFinalBlock(new byte[]{},0,0);
+                    MapSha = sha.Hash;
                 }
             }
+
+
+            System.Console.WriteLine(BitConverter.ToString(MapInfo).Replace("-",""));
+            System.Console.WriteLine(BitConverter.ToString(MapCRC).Replace("-",""));
+            System.Console.WriteLine(BitConverter.ToString(MapSha).Replace("-",""));
             Thread.Sleep(10000);
+        }
+
+
+        public uint ROTL(uint i,int c){
+            return (i << c) | (i >> (32 - c));
+        }
+        public uint XORRotateLeft(byte[] d){
+            uint i=0;
+            uint v=0;
+            int l=d.Length;
+
+            if(l>3){
+                while(i<l-3){
+                    v = ROTL(v ^ (((uint)d[i]) + ((uint)d[i+1]<<8) + ((uint)d[i+2]<<16) + ((uint)d[i+3]<<24)), 3);
+                    i+=4;
+                }
+            }
+
+            while(i<l){
+                v = ROTL(v ^ d[i], 3);
+                i++;
+            }
+
+            return v;
         }
     }
 }
