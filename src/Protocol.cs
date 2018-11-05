9968418ead69b3b7416a2f5f4a4ab2d22083b3c1
Sepehr Ahmadi <business@gocode.it>
2018-11-05 05:55:52 -0500

    Many many more packets supported

--- src/Protocol.cs
+++ src/Protocol.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System;
 
 using Banshee.packets;
 
@@ -9,11 +10,46 @@ namespace Banshee
         public static IPacket[] packets = new IPacket[0xff];
 
         public static void init(){
+            initTCP();
             initUDP();
         }
+        
+        public static byte[] preparePacket(IPacket o){
+            byte[] data = o.toBytes();
+            int size = data.Length;
+            byte[] arr = new byte[size+4];
 
-        public static void initUDP(){
+            arr[0] = 0xf7;
+            arr[1] = packetId(o);
+
+            int pktsize = size+4;            
+            arr[2] = (byte)(pktsize&0xff);
+            arr[3] = (byte)(pktsize>>8);
+
+            Array.Copy(data,0,arr,4,size);
+            return arr;
+        }
+
+        public static void initTCP(){
+            packets[0x01] = new x01PINGFROMHOST(); 
+            packets[0x04] = new x04SLOTINFOJOIN(); 
+            packets[0x05] = new x05REJECTJOIN(); 
+            packets[0x06] = new x06PLAYERINFO(); 
+            packets[0x07] = new x07PLAYERLEAVE(); 
+            packets[0x09] = new x09SLOTINFO(); 
+            packets[0x0F] = new x0fCHATFROMHOST(); 
+
+            packets[0x1E] = new x1eJOINREQUEST(); 
+            
+            packets[0x28] = new x28CHATTOHOST(); 
+            packets[0x3d] = new x3dMAPCHECK(); 
+
+            packets[0x42] = new x42MAPSIZEVERIFY(); 
+            packets[0x46] = new x46PONGTOHOST(); 
+        }
+        public static void initUDP(){            
             packets[0x2F] = new x2fREQUESTGAME(); 
+
             packets[0x30] = new x30GAMEDETAILS(); 
             packets[0x31] = new x31CREATEGAME(); 
             packets[0x32] = new x32REFRESHGAME(); 
